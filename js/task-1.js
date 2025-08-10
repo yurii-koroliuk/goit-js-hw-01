2ab0b86b3811d9f2e047d8a45e10345294f63358
Yurii <ikorolyuk78@gmail.com>
2025-08-10 21:56:42 +0300

    add js content

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,6 @@
 function makeTransaction(quantity, pricePerDroid) {
-  //
+  const totalPrice = quantity * pricePerDroid;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
